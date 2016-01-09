1edf1afd8035ffddc00a7b3279fae3c273a189ad
Akshit Arora <akshit.arora1995@gmail.com>
2016-01-09 17:15:00 +0530

    initial_commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+<meta charset="utf-8">
+<script>document.documentElement.className="js"</script>
+<title>Akshit Arora</title>
+<link href="//fonts.googleapis.com/css?family=IM+Fell+DW+Pica&subset=latin" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<section id="wrapper">
+<h1><span class="firstname">Akshit<span class="sz">Arora</span></span><span class="nbsp">&nbsp;</span><span class="lastname"><span class="sz">Arora</span></span></h1>
+<h2>
+    Pre-final Year<br>
+    B.E. Computer Engineering<br>
+    Thapar University, Patiala<br>
+    India<br><br>
+</h2>
+<h3>TravelBuff . OpenSourceGuy . Photographer . Explorer . Researcher<br>
+    <a href="temp/index.html">Know More</a></h3>
+</section>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>
+    $(function(){
+        $("h1").delay(500).fadeIn(1000);
+        $("h2").delay(1500).fadeIn(1000);
+        $("h3").delay(2500).fadeIn(1000);
+    });
+</script>
+</body>
+</html>
